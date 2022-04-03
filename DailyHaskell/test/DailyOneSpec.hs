f28025d7c85e689ce53ddeb7cc0e9a039844154f
George Kreye <george.kreye@du.edu>
2022-04-02 22:01:34 -0600

    Fix malformed tests

--- DailyHaskell/test/DailyOneSpec.hs
+++ DailyHaskell/test/DailyOneSpec.hs
@@ -11,8 +11,8 @@ spec = do
         it "produces the quadratic 0" $
             quadratic 0 0 0 0 `shouldBe` 0
         
-        it "produces the quadratic 4" $
-            quadratic 1 1 1 1 `shouldBe` 4
+        it "produces the quadratic 3" $
+            quadratic 1 1 1 1 `shouldBe` 3
 
     describe "scaleVector" $ do
         it "produces the scaled vector (2,2)" $
@@ -20,4 +20,4 @@ spec = do
 
     describe "tripleDistance" $ do
         it "produces the distance 1.73205080757" $
-            tripleDistance (1,1,1) (2,2,2) `shouldBe` 1.73205080757
+            tripleDistance (1,1,1) (2,2,2) `shouldBe` sqrt(3)
