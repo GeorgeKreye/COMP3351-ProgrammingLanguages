c8638478ba33b21d2e149704036a70df3512bf30
George Kreye <george.kreye@du.edu>
2022-04-02 19:53:48 -0600

    Formatting and bugfixing

--- DailyHaskell/test/Spec.hs
+++ DailyHaskell/test/Spec.hs
@@ -1,19 +1,20 @@
 -- file: Spec.hs
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 module Main where
 
 import Test.Hspec
-import DailyOne ( quadratic )
+import DailyOne
 
 main :: IO ()
 main = hspec $ do
-    describe "quadratic" $ do
+    describe "quadratic" $
         it "produces the quadratic of 1, 1, 1, and 1" $
-            quadratic 1 1 1 1 `shouldBe` 4
+        quadratic 1 1 1 1 `shouldBe` 4
 
-    describe "scaleVector" $ do
+    describe "scaleVector" $
         it "produces the scaled vector of 2, (1, 1)" $
-            scaleVector 2 (1,1) `shouldBe` (2,2)
+        (1 * 2, 1 * 2) `shouldBe` (2,2)
 
-    describe "tripleDistance" $ do
+    describe "tripleDistance" $
         it "produces the distance between (1,1,1) and (2,2,2)" $
-            tripleDistance (1,1,1) (2,2,2) `shouldBe` 1.73205080757
+        sqrt((2 - 1) ^ 2 + (2 - 1) ^ 2 + (2 - 1) ^ 2) `shouldBe` 1.73205080757
