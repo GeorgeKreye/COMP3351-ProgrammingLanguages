57b7b8e75819ddc2da50a7f19171ce1d76e9c802
George Kreye <george.kreye@du.edu>
2022-04-02 19:43:09 -0600

    add tripleDistance test to test file

--- DailyHaskell/test/Spec.hs
+++ DailyHaskell/test/Spec.hs
@@ -13,4 +13,7 @@ main = hspec $ do
     describe "scaleVector" $ do
         it "produces the scaled vector of 2, (1, 1)" $
             scaleVector 2 (1,1) `shouldBe` (2,2)
-    
+
+    describe "tripleDistance" $ do
+        it "produces the distance between (1,1,1) and (2,2,2)" $
+            tripleDistance (1,1,1) (2,2,2) `shouldBe` 1.73205080757
