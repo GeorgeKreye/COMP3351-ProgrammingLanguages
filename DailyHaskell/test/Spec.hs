7dc95b47efe050714a1d641f5a975550115708ef
George Kreye <george.kreye@du.edu>
2022-04-02 19:36:59 -0600

    Test files

--- DailyHaskell/test/Spec.hs
+++ DailyHaskell/test/Spec.hs
@@ -1,2 +1,16 @@
+-- file: Spec.hs
+module Main where
+
+import Test.Hspec
+import DailyOne ( quadratic )
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "quadratic" $ do
+        it "produces the quadratic of 1, 1, 1, and 1" $
+            quadratic 1 1 1 1 `shouldBe` 4
+
+    describe "scaleVector" $ do
+        it "produces the scaled vector of 2, (1, 1)" $
+            scaleVector 2 (1,1) `shouldBe` (2,2)
+    
