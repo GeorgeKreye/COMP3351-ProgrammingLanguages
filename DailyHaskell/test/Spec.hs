23fca465709829573abf5a0d90384f0242cf3b5b
George Kreye <george.kreye@du.edu>
2022-04-04 14:38:26 -0600

    mirror DailyTwo branch

--- DailyHaskell/test/Spec.hs
+++ DailyHaskell/test/Spec.hs
@@ -1,3 +1,2 @@
-module Spec where -- < needs to be removed to be usable for some reason
-    {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
-    --TODO: find out why this doesn't compile, but the one that has a ioctl error does
+--TODO: find out why this does not work
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
