2efe47680431ea5263f21f8da5f8e5f8b2da0300
George Kreye <george.kreye@du.edu>
2022-04-04 14:23:50 -0600

    removing error for now, this fix still won't run

--- DailyHaskell/test/Spec.hs
+++ DailyHaskell/test/Spec.hs
@@ -1,1 +1,3 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Spec where -- < needs to be removed to be usable for some reason
+    {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+    --TODO: find out why this doesn't compile, but the one that has a ioctl error does
