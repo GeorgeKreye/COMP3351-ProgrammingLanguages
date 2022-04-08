af4d23e76cc962dec9a2db6f003f3a6a818e7743
George Kreye <george.kreye@du.edu>
2022-04-08 09:51:19 -0600

    Add countOccurences and comments

--- DailyHaskell/src/DailyThree.hs
+++ DailyHaskell/src/DailyThree.hs
@@ -1,6 +1,19 @@
 module DailyThree where
+    {- 
+        Given an element and a list, removes all elements that are not the
+        given element and returns the resulting list
+    -}
     removeAllExcept :: Eq a => a -> [a] -> [a]
     removeAllExcept e l
       | null l = []
       | head l == e = head l : removeAllExcept e (tail l)
       | otherwise = removeAllExcept e (tail l)
+    {- 
+        Given an element and a list, returns the count of occurences of
+        that element in the list
+    -}
+    countOccurences :: Eq a => a -> [a] -> Int
+    countOccurences e l
+      | null l = 0
+      | head l == e = countOccurences e (tail l) + 1
+      | otherwise = countOccurences e (tail l)
--- DailyHaskell/test/DailyThreeSpec.hs
+++ DailyHaskell/test/DailyThreeSpec.hs
@@ -10,3 +10,8 @@ module DailyThreeSpec where
                 removeAllExcept 'a' ['b','a','c','a'] `shouldBe` "aa"
             it "produces the list [1]" $
                 removeAllExcept 1 [2,3,4,1] `shouldBe` [1]
+        describe "countOccurences" $ do
+            it "produces the integer 2" $
+                countOccurences 'a' ['a','b','c','a'] `shouldBe` 2
+            it "produces the integer 0" $
+                countOccurences 1 [2,4,5,2] `shouldBe` 0
