a23f9a6dc3ddbefa92f4ef9ebd13b919c5fc5b25
George Kreye <george.kreye@du.edu>
2022-04-11 15:19:13 -0600

    add unzipTriples

--- DailyHaskell/src/DailyFour.hs
+++ DailyHaskell/src/DailyFour.hs
@@ -5,7 +5,31 @@ module DailyFour where
         Assumes all lists are the same size.
     -}
     zip3Lists :: [a1] -> [a2] -> [a3] -> [(a1, a2, a3)]
-    zip3Lists a b c = 
+    zip3Lists a b c =
         if null a
             then []
             else (head a, head b, head c) : zip3Lists (tail a) (tail b) (tail c)
+    {- 
+        Given a list of triples, creates a tuple of three lists, containing
+        the first elements, second elements, and third elements of each
+        tuple respectively
+    -}
+    unzipTriples :: [(a,b,c)] -> ([a],[b],[c])
+    unzipTriples t = (firstValues t, secondValues t, thirdValues t)
+    -- utility functions for unzipTriples
+    getFirstTriple :: (a,b,c) -> a
+    getFirstTriple (a,_,_) = a
+    getSecondTriple :: (a, b, c) -> b
+    getSecondTriple (_,b,_) = b
+    getThirdTriple :: (a, b, c) -> c
+    getThirdTriple (_,_,c) = c
+    firstValues :: [(a, b, c)] -> [a]
+    firstValues [] = []
+    firstValues t = getFirstTriple (head t) : firstValues (tail t)
+    secondValues :: [(a, b, c)] -> [b]
+    secondValues [] = []
+    secondValues t = getSecondTriple (head t) : secondValues (tail t)
+    thirdValues :: [(a,b,c)] -> [c]
+    thirdValues [] = []
+    thirdValues t = getThirdTriple (head t) : thirdValues (tail t)
+    
--- DailyHaskell/test/DailyFourSpec.hs
+++ DailyHaskell/test/DailyFourSpec.hs
@@ -10,3 +10,8 @@ module DailyFourSpec where
                 zip3Lists [1,2,3] ['a','b','c'] [4,5,6] `shouldBe` [(1,'a',4),(2,'b',5),(3,'c',6)]
             it "produces the list [(10,'v',0),(15,'w',1),(20,'x',0),(25,'y',0),(30,'z',1)]" $
                 zip3Lists [10,15,20,25,30] ['v','w','x','y','z'] [0,1,0,0,1] `shouldBe` [(10,'v',0),(15,'w',1),(20,'x',0),(25,'y',0),(30,'z',1)]
+        describe "unzipTriples" $ do
+            it "produces the tuple ([1,4,7],[2,5,8],[3,6,9])" $
+                unzipTriples [(1,2,3),(4,5,6),(7,8,9)] `shouldBe` ([1,4,7],[2,5,8],[3,6,9])
+            it "produces the tuple ([1,4,7,10,13],[2,5,8,11,14],[3,6,9,12,15])" $
+                unzipTriples [(1,2,3),(4,5,6),(7,8,9),(10,11,12),(13,14,15)] `shouldBe` ([1,4,7,10,13],[2,5,8,11,14],[3,6,9,12,15])
