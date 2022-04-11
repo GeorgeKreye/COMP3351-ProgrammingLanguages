986d9ae42dcacced106d161e0fa72c01327fb374
George Kreye <george.kreye@du.edu>
2022-04-11 14:22:20 -0600

    add Zip3Lists

--- DailyHaskell/src/DailyFour.hs
+++ DailyHaskell/src/DailyFour.hs
@@ -1,2 +1,11 @@
 module DailyFour where
-    
+    {- 
+        Given 3 lists, creates a list of tuples where each tuple contains
+        the nth element of the list.
+        Assumes all lists are the same size.
+    -}
+    zip3Lists :: [a1] -> [a2] -> [a3] -> [(a1, a2, a3)]
+    zip3Lists a b c = 
+        if null a
+            then []
+            else (head a, head b, head c) : zip3Lists (tail a) (tail b) (tail c)
--- DailyHaskell/test/DailyFourSpec.hs
+++ DailyHaskell/test/DailyFourSpec.hs
@@ -2,3 +2,11 @@
 module DailyFourSpec where
     import Test.Hspec
     import DailyFour
+
+    spec :: Spec 
+    spec = do
+        describe "zip3Lists" $ do
+            it "produces the list [(1,'a',4),(2,'b',5),(3,'c',6)]" $
+                zip3Lists [1,2,3] ['a','b','c'] [4,5,6] `shouldBe` [(1,'a',4),(2,'b',5),(3,'c',6)]
+            it "produces the list [(10,'v',0),(15,'w',1),(20,'x',0),(25,'y',0),(30,'z',1)]" $
+                zip3Lists [10,15,20,25,30] ['v','w','x','y','z'] [0,1,0,0,1] `shouldBe` [(10,'v',0),(15,'w',1),(20,'x',0),(25,'y',0),(30,'z',1)]
