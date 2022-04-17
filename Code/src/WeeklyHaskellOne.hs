e257391f69ff0d95214eb5b45d5ba27f709754dc
George Kreye <george.kreye@du.edu>
2022-04-17 14:55:31 -0600

    comment for removeChar

--- Code/src/WeeklyHaskellOne.hs
+++ Code/src/WeeklyHaskellOne.hs
@@ -1,7 +1,10 @@
 module WeeklyHaskellOne where
+    {- 
+        Given a char and a string, removes all instances of the given char
+        and returns a string
+    -}
     removeChar :: Eq a => a -> [a] -> [a]
     removeChar c s
       | null s = []
       | head s == c = removeChar c (tail s)
       | otherwise = head s : removeChar c (tail s)
-    
