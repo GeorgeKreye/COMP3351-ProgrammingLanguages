64198578789a002099ff122225adfa2b1a496822
George Kreye <george.kreye@du.edu>
2022-04-17 14:53:55 -0600

    Implement removeChar

--- Code/src/WeeklyHaskellOne.hs
+++ Code/src/WeeklyHaskellOne.hs
@@ -1,2 +1,7 @@
 module WeeklyHaskellOne where
+    removeChar :: Eq a => a -> [a] -> [a]
+    removeChar c s
+      | null s = []
+      | head s == c = removeChar c (tail s)
+      | otherwise = head s : removeChar c (tail s)
     
--- Code/test/WeeklyHaskellOneSpec.hs
+++ Code/test/WeeklyHaskellOneSpec.hs
@@ -2,3 +2,13 @@
 module WeeklyHaskellOneSpec where
     import Test.Hspec
     import WeeklyHaskellOne
+    
+    spec :: Spec
+    spec = do 
+        describe "removeChar" $ do
+            it "produces the string \"heo\"" $
+                removeChar 'l' "hello" `shouldBe` "heo"
+            it "produces the string \"Sea\"" $
+                removeChar 's' "Seas" `shouldBe` "Sea"
+            it "produces the string \"car\"" $
+                removeChar 't' "catr" `shouldBe` "car"
