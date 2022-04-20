f032451d146534cecdd8ed29a6a5b2adf6d06649
George Kreye <george.kreye@du.edu>
2022-04-20 17:31:10 -0600

    Changed certain test suites to have empty input test cases

--- Code/test/WeeklyHaskellOneSpec.hs
+++ Code/test/WeeklyHaskellOneSpec.hs
@@ -10,27 +10,27 @@ module WeeklyHaskellOneSpec where
                 removeChar 'l' "hello" `shouldBe` "heo"
             it "produces the string \"Sea\"" $
                 removeChar 's' "Seas" `shouldBe` "Sea"
-            it "produces the string \"car\"" $
-                removeChar 't' "catr" `shouldBe` "car"
+            it "produces the string \"\"" $
+                removeChar 't' "" `shouldBe` ""
         describe "removeWhitespace" $ do
             it "produces the string \"nosun\"" $
                 removeWhitespace "no sun\n" `shouldBe` "nosun"
             it "produces the string \"carsandruckes\"" $
                 removeWhitespace "cars and\trucke\rs" `shouldBe` "carsandruckes"
             it "produces the string \"\"" $
-                removeWhitespace "\n \t \r" `shouldBe` ""
+                removeWhitespace "" `shouldBe` ""
         describe "removePunctuation" $ do
             it "produces the string \"Hello My name is Joe\"" $
                 removePunctuation "Hello! My name is Joe." `shouldBe` "Hello My name is Joe"
             it "produces the string \"Can I ask a question\"" $
                 removePunctuation "(Can I ask a) question?" `shouldBe` "Can I ask a question"
-            it "produces the string \"Includes pencils paper and an eraser\"" $
-                removePunctuation "Includes: pencils, paper, and an eraser" `shouldBe` "Includes pencils paper and an eraser"
+            it "produces the string \"\"" $
+                removePunctuation "" `shouldBe` ""
         describe "charsToAscii" $ do
             it "produces the list [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]" $
                 charsToAscii "Hello world" `shouldBe` [72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]
-            it "produces the list [88, 120, 80, 114, 111, 71, 97, 109, 101, 114, 55, 55, 120, 88]" $
-                charsToAscii "XxProGamer77xX" `shouldBe` [88, 120, 80, 114, 111, 71, 97, 109, 101, 114, 55, 55, 120, 88]
+            it "produces the list []" $
+                charsToAscii "" `shouldBe` []
             it "produces the list [82, 97, 100, 105, 99, 97, 108, 33]" $
                 charsToAscii "Radical!" `shouldBe` [82, 97, 100, 105, 99, 97, 108, 33]
         describe "asciiToChars" $ do
