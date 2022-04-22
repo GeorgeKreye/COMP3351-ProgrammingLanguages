03424cc5646107526f55a30d1927bfa42d9a3a85
George Kreye <george.kreye@du.edu>
2022-04-22 10:30:06 -0600

    Add search; yet to be implemented

--- Code/src/TriTree.hs
+++ Code/src/TriTree.hs
@@ -1,5 +1,11 @@
 module TriTree where
-    data TriTree a = Empty | 
-                 NodeOne a (TriTree a) (TriTree a) (TriTree a) | 
-                 NodeTwo a a (TriTree a) (TriTree a) (TriTree a) 
+    data TriTree a = Empty |
+                 NodeOne a (TriTree a) (TriTree a) (TriTree a) |
+                 NodeTwo a a (TriTree a) (TriTree a) (TriTree a)
                  deriving (Show)
+    {-
+        Given a value and a TriTree, returns a boolean of whether
+        the TriTree contains the value
+    -}
+    search :: Ord v => v -> TriTree t -> Bool
+    search v t = False;
