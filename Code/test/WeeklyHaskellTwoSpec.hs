12ba3cdbb2922cd5c376e6ffc75db3188f6509ff
George Kreye <george.kreye@du.edu>
2022-04-22 10:07:07 -0600

    imports and placeholder for test suite

--- Code/test/WeeklyHaskellTwoSpec.hs
+++ Code/test/WeeklyHaskellTwoSpec.hs
@@ -1,2 +1,10 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 module WeeklyHaskellTwoSpec where
+    import Test.Hspec
+    import WeeklyHaskellTwo
+
+    spec :: Spec 
+    spec = do
+        describe "" $ do
+            it "" $
+               1+1 `shouldBe` 2
