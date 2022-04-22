799f07627ef219fc9f3adecbbc28954b8d33dcc0
George Kreye <george.kreye@du.edu>
2022-04-22 09:11:26 -0600

    Implement multPairs

--- Code/src/DailyHaskellFive.hs
+++ Code/src/DailyHaskellFive.hs
@@ -1,2 +1,9 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use uncurry" #-}
 module DailyHaskellFive where
-    
+    {-
+        Given a list of pairs of integers, returns a list of the
+        products of each pair
+    -}
+    multPairs :: [(Integer,Integer)] -> [Integer]
+    multPairs = map (\(x,y) -> x * y)
--- Code/test/DailyHaskellFiveSpec.hs
+++ Code/test/DailyHaskellFiveSpec.hs
@@ -2,3 +2,12 @@
 module DailyHaskellFiveSpec where
     import Test.Hspec
     import DailyHaskellFive
+    spec :: Spec
+    spec = do
+        describe "multPairs" $ do
+            it "produces the list []" $
+                multPairs [] `shouldBe` []
+            it "produces the list [15, 81, 48, 32]" $
+                multPairs [(3,5),(9,9),(6,8),(4,8)] `shouldBe` [15, 81, 48, 32]
+            it "produces the list [-20, 0]" $
+                multPairs [(-4,5),(0,7)] `shouldBe` [-20, 0]
