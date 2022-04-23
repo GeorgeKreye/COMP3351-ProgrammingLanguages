c7c5a4b973f8eb173d74558a5ea1ebf9d5db9015
George Kreye <george.kreye@du.edu>
2022-04-23 13:02:23 -0600

    Start work on implementing insert

--- Code/src/TriTree.hs
+++ Code/src/TriTree.hs
@@ -6,7 +6,6 @@ module TriTree where
     {-
         Given a value and a TriTree, returns a boolean of whether
         the TriTree contains the value
-        TODO: implement
     -}
     search :: Ord a => a -> TriTree a -> Bool
     search _ Empty = False
@@ -14,3 +13,17 @@ module TriTree where
         (x == v) || (search v l || search v m || search v r)
     search v (NodeTwo x y l m r) =
         (x == v || y == v) || (search v l || search v m || search v r)
+    {- 
+        Given a value and a TriTree, inserts the value as new NodeOne leaf in the proper order
+        TODO: Fix error
+    -}
+    insert :: Ord a => a -> TriTree a -> TriTree a
+    insert v Empty = NodeOne v Empty Empty Empty
+    insert v (NodeOne x l m r) =
+        if v < x
+            then NodeOne x (insert v l) m r
+            else NodeOne x l (insert v m) r
+    insert v (NodeTwo x y l m r)
+      | v < x = NodeTwo x y (insert v l) m r
+      | v >= x && v < y = NodeTwo x y l (insert v m) r
+      | otherwise = NodeTwo x y l m (insert v r)
--- Code/test/TriTreeSpec.hs
+++ Code/test/TriTreeSpec.hs
@@ -6,9 +6,12 @@ module TriTreeSpec where
     spec :: Spec
     spec = do
         describe "search" $ do
-        it "produces the boolean False" $
-            search 1 Empty `shouldBe` False
-        it "produces the boolean True" $
-            search 5 (NodeOne 2 (NodeTwo 5 9 (NodeOne 7 Empty Empty Empty) (NodeOne 4 Empty Empty Empty) Empty) (NodeOne 3 Empty Empty Empty) Empty) `shouldBe` True
-        it "produces the boolean False" $
-            search 80 (NodeOne 2 (NodeTwo 5 9 (NodeOne 7 Empty Empty Empty) (NodeOne 4 Empty Empty Empty) Empty) (NodeOne 3 Empty Empty Empty) Empty) `shouldBe` False
+            it "produces the boolean False" $
+                search 1 Empty `shouldBe` False
+            it "produces the boolean True" $
+                search 5 (NodeOne 2 (NodeTwo 5 9 (NodeOne 7 Empty Empty Empty) (NodeOne 4 Empty Empty Empty) Empty) (NodeOne 3 Empty Empty Empty) Empty) `shouldBe` True
+            it "produces the boolean False" $
+                search 80 (NodeOne 2 (NodeTwo 5 9 (NodeOne 7 Empty Empty Empty) (NodeOne 4 Empty Empty Empty) Empty) (NodeOne 3 Empty Empty Empty) Empty) `shouldBe` False
+        describe "insert" $
+            it "produces the TriTree [2(1()1 3()3)2]" $
+                insert 1 (NodeOne 2 Empty (NodeOne 3 Empty Empty Empty) Empty) `shouldBe ` (NodeOne 2 (NodeOne 1 Empty Empty Empty) (NodeOne 3 Empty Empty Empty) Empty)
