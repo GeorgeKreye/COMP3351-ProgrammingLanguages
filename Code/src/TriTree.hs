452cef66cd33c307b93fa30543c53d3ef30abeda
George Kreye <george.kreye@du.edu>
2022-04-23 12:39:51 -0600

    Implement search

--- Code/src/TriTree.hs
+++ Code/src/TriTree.hs
@@ -1,12 +1,16 @@
 module TriTree where
     data TriTree a = Empty |
-                 NodeOne a (TriTree a) (TriTree a) (TriTree a) |
-                 NodeTwo a a (TriTree a) (TriTree a) (TriTree a)
-                 deriving (Show)
+        NodeOne a (TriTree a) (TriTree a) (TriTree a) |
+        NodeTwo a a (TriTree a) (TriTree a) (TriTree a)
+        deriving (Show)
     {-
         Given a value and a TriTree, returns a boolean of whether
         the TriTree contains the value
         TODO: implement
     -}
-    search :: Ord v => v -> TriTree t -> Bool
-    search v t = False;
+    search :: Ord a => a -> TriTree a -> Bool
+    search _ Empty = False
+    search v (NodeOne x l m r)=
+        (x == v) || (search v l || search v m || search v r)
+    search v (NodeTwo x y l m r) =
+        (x == v || y == v) || (search v l || search v m || search v r)
--- Code/test/TriTreeSpec.hs
+++ Code/test/TriTreeSpec.hs
@@ -6,5 +6,9 @@ module TriTreeSpec where
     spec :: Spec
     spec = do
         describe "search" $ do
-            it "produces the boolean False" $
-                search 1 Empty `shouldBe` False
+        it "produces the boolean False" $
+            search 1 Empty `shouldBe` False
+        it "produces the boolean True" $
+            search 5 (NodeOne 2 (NodeTwo 5 9 (NodeOne 7 Empty Empty Empty) (NodeOne 4 Empty Empty Empty) Empty) (NodeOne 3 Empty Empty Empty) Empty) `shouldBe` True
+        it "produces the boolean False" $
+            search 80 (NodeOne 2 (NodeTwo 5 9 (NodeOne 7 Empty Empty Empty) (NodeOne 4 Empty Empty Empty) Empty) (NodeOne 3 Empty Empty Empty) Empty) `shouldBe` False
