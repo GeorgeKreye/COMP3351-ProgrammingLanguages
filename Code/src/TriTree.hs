158c2e17726a72edbe19d5aefed9d8d5988a69f3
George Kreye <george.kreye@du.edu>
2022-04-23 18:03:01 -0600

    start tinkering branch

--- Code/src/TriTree.hs
+++ Code/src/TriTree.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use foldl" #-}
 module TriTree where
+    -- Hopefully error(s) isn't/aren't in here or that's a fundamental issue
     data TriTree a = Empty |
         NodeOne a (TriTree a) (TriTree a) (TriTree a) |
         NodeTwo a a (TriTree a) (TriTree a) (TriTree a)
