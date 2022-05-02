2ce1a80aa43e764e7d68a5706f11245bd58e289d
George Kreye <george.kreye@du.edu>
2022-05-02 15:56:59 -0600

    foldr rewrite

--- Code/src/DailyHaskellSix.hs
+++ Code/src/DailyHaskellSix.hs
@@ -1,5 +1,7 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use lambda-case" #-}
+{-# HLINT ignore "Replace case with fromMaybe" #-}
 module DailyHaskellSix where
-    import Data.Maybe (isJust, fromMaybe, isNothing)
     {- 
         Given an integer and a list of words, filters said list to include only words that are
         shorten than or equal to the given integer in length
@@ -16,21 +18,29 @@ module DailyHaskellSix where
         Given a list of Maybe instances, filters said list to remove any values of Nothing 
     -}
     onlyJust :: [Maybe a] -> [Maybe a]
-    onlyJust = filter isJust
+    onlyJust = filter (\ e ->
+        case e of 
+            Just _ -> True
+            Nothing -> False)
     {- 
         Given a function of type (a -> Maybe b) and a list of type [a], calls the function on each element of the list
         If the result of the function being called is Nothing, return Nothing; otherwise return a list of results wrapped by Just
     -}
     allAnswers :: (a -> Maybe b) -> [a] -> Maybe [b]
     allAnswers _ [] = Just []
-    allAnswers f (x:xs) =
-        let r = f x
-            p = allAnswers f xs
-            pV = fromMaybe [] p
-        in
-            if isNothing r || isNothing p
-                then Nothing
-                else Just (fromMaybe (head pV) r : pV) -- (head pV) should never be used, just a type placeholder
+    allAnswers f l =
+        foldr (\e a ->
+            let
+                r = f e
+            in
+                case r of 
+                    Nothing -> Nothing
+                    Just v -> 
+                        case a of
+                            Nothing -> Nothing
+                            Just aV -> Just (v : aV)
+
+            ) Nothing l
     -- utility function for testing
     invert :: (Eq a, Fractional a) => a -> Maybe a
     invert d =
