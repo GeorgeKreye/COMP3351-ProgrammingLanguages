993bb2928311cce763164c756b3971f02470831c
George Kreye <george.kreye@du.edu>
2022-05-04 13:55:27 -0600

    rm no-longer-applicable line

--- Code/src/DailyHaskellSeven.hs
+++ Code/src/DailyHaskellSeven.hs
@@ -2,7 +2,6 @@ module DailyHaskellSeven where
     {- 
         Given a foldable list of lists, creates a single list containing all elements
         contained in the list of lists
-        TODO: Find correct function
     -}
     createOneList :: Foldable t => t [[a]] -> [a]
     createOneList = foldr (\e a -> getList e ++ a) []
