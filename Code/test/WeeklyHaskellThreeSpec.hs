ebda034d8010807b48e1f352bbe1b9b9bccfc63d
George Kreye <george.kreye@du.edu>
2022-05-11 14:44:57 -0600

    Attempting to build Vec typeclass

--- Code/test/WeeklyHaskellThreeSpec.hs
+++ Code/test/WeeklyHaskellThreeSpec.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-type-defaults #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-} -- until issue solved
 module WeeklyHaskellThreeSpec where
     import Test.Hspec
     import WeeklyHaskellThree
@@ -6,5 +7,5 @@ module WeeklyHaskellThreeSpec where
     spec :: Spec
     spec = do
         describe "Vec3-Show" $ do
-            it "produces the string \"Vec3 [1.0, 2.0, 3.0]\"" $
-                print (Vec3 [1.0, 2.0, 3.0]) `shouldBe` "Vec3 [1.0, 2.0, 3.0]"
+            it "produces the string \"Vec [1.0, 2.0, 3.0, 4.0]\"" $
+                print (Vec [1.0, 2.0, 3.0, 4.0]) `shouldBe` "Vec [1.0, 2.0, 3.0, 4.0]"
