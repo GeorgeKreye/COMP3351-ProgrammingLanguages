d0c4eec67ab1f8edc7980a79cc5124f1f81c118a
George Kreye <george.kreye@du.edu>
2022-05-11 15:30:39 -0600

    Tests for multiplying vectors

--- Code/test/WeeklyHaskellThreeSpec.hs
+++ Code/test/WeeklyHaskellThreeSpec.hs
@@ -5,13 +5,18 @@ module WeeklyHaskellThreeSpec where
 
     spec :: Spec
     spec = do
-        describe "Vec3 - Show" $ do
+        describe "Vec - Show" $ do
             it "produces the string \"Vec [1.0,2.0,3.0,4.0]\"" $
                 show (Vec [1.0, 2.0, 3.0, 4.0]) `shouldBe` "Vec [1.0,2.0,3.0,4.0]"
             it "produces the string \"Vec []\"" $
                 show (Vec []) `shouldBe` "Vec []"
-        describe "Vec3 - (+)" $ do
+        describe "Vec - (+)" $ do
             it "produces Vec []" $
                 Vec [] + Vec [1.0,2.0] `shouldBe` Vec []
             it "produces Vec [2.0,3.0]" $
-                Vec [1.0,2.0] + Vec [1.0,1.0] `shouldBe` Vec [2.0,3.0]    
+                Vec [1.0,2.0] + Vec [1.0,1.0] `shouldBe` Vec [2.0,3.0]
+        describe "Vec - (*)" $ do
+            it "produces Vec [4.0,9.0,16.0]" $
+                Vec [2.0,3.0,4.0] * Vec [2.0,3.0,4.0] `shouldBe` Vec [4.0,9.0,16.0]
+            it "produces Vec []" $
+                Vec [] * Vec [1.0] `shouldBe` Vec []
