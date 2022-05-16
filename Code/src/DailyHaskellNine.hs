c44b119f149bef435bef78cc082a86856c5cade7
George Kreye <george.kreye@du.edu>
2022-05-16 14:57:05 -0600

    Implement secondFunctorLaw

--- Code/src/DailyHaskellNine.hs
+++ Code/src/DailyHaskellNine.hs
@@ -9,3 +9,9 @@ module DailyHaskellNine where
     -}
     firstFunctorLaw :: (Eq (f a), Functor f) => f a -> Bool
     firstFunctorLaw f = fmap id f == id f
+    {- 
+        Given (b -> c) and (a -> c) where Eq (f c) and f is a functor, returns
+        True if the second functor law is upheld
+    -}
+    secondFunctorLaw :: (Eq (f c), Functor f) => (b -> c) -> (a -> b) -> f a -> Bool
+    secondFunctorLaw f g functor = fmap (f . g) functor == fmap f (fmap g functor)
--- Code/test/DailyHaskellNineSpec.hs
+++ Code/test/DailyHaskellNineSpec.hs
@@ -2,6 +2,7 @@
 module DailyHaskellNineSpec where
     import Test.Hspec
     import DailyHaskellNine
+    import Data.Char
 
     spec :: Spec
     spec = do 
@@ -10,3 +11,8 @@ module DailyHaskellNineSpec where
                 firstFunctorLaw (Just ('c',35)) `shouldBe` True
             it "produces the boolean True" $
                 firstFunctorLaw [2,3,5,7,11] `shouldBe` True
+        describe "secondFunctorLaw" $ do
+            it "produces the boolean True" $
+                secondFunctorLaw isAlpha fst (Just('c',35)) `shouldBe` True
+            it "produces the boolean True" $
+                secondFunctorLaw chr (+96) [2,3,5,7,11] `shouldBe` True
